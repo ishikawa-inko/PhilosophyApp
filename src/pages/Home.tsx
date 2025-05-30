2602f119c850c3819647a494d822939e35bf8b07
ishikawa-inko <isimasa727@icloud.com>
2025-05-30 14:18:19 +0900

    背景追加

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
 import '../styles/Home.css'; //CSSファイルをインポート
 
+
 //Homeコンポーネントを定義
 export default function Home() {
   return (
+    
     <div className="container">
       <h2 className="subtitle">哲学者にお悩み相談？！</h2>
       <h1 className="title">思想家に訊け！お悩み相談会</h1>
