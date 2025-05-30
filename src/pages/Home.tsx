aa4e71a22d84e0d4652b770d6f99a4dac7324fa6
ishikawa-inko <isimasa727@icloud.com>
2025-05-30 13:56:33 +0900

    Home画面変更

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,22 @@
-import '../styles/Home.css'; // 相対パスで読み込む
+import '../styles/Home.css'; //CSSファイルをインポート
 
+//Homeコンポーネントを定義
 export default function Home() {
   return (
     <div className="container">
-      <h2 className="subtitle">
-        哲学者にお悩み相談？！<br />
-      </h2>
-      <h1 className="title">哲学の部屋</h1>
+      <h2 className="subtitle">哲学者にお悩み相談？！</h2>
+      <h1 className="title">思想家に訊け！お悩み相談会</h1>
+      
+      
       <button className="button" onClick={() => window.location.href = '/input'}>
         悩みを入力
       </button>
+
+      {/* モデレーターインコ */}
+      <div className="inco-wrapper">
+        <div className="inco-speech">ようこそ、悩める魂よ！</div>
+      </div>
+      
     </div>
   );
 }
