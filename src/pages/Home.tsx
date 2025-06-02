bf32d4daad62b47b40c9a6066b410469fb777166
ishikawa-inko <isimasa727@icloud.com>
2025-06-02 14:49:27 +0900

    最終調整

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,8 @@ import birdImage from '../assets/photo/bird.png';
 export default function Home() {
   return (
     <div className="container">
-      <h2 className="subtitle">哲学者にお悩み相談？！</h2>
-      <h1 className="title">思想家に訊け！お悩み相談会</h1>
+      <h2 className="subtitle">哲学お悩み相談？！</h2>
+      <h1 className="title">哲学者に訊け！お悩み相談会</h1>
 
       <button className="button" onClick={() => window.location.href = '/input'}>
         悩みを入力
